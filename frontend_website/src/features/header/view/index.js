3e657ba4bc415f931436f3349cf8b9682b8a7674
Trung Phan <kaitrung99@gmail.com>
2020-10-13 18:41:44 +0700

    Update Header

--- frontend_website/src/features/header/view/index.js
+++ frontend_website/src/features/header/view/index.js
@@ -1,9 +1,37 @@
 import React, { Component } from 'react';
+import { Menu } from 'antd';
 
+
+import {SettingOutlined } from '@ant-design/icons';
+import 'antd/dist/antd.css';
+import "./style.css";
+const { SubMenu } = Menu;
 class Header extends Component {
+ 
   render() {
     return (
-      <div> Header đây </div>
+      <div  className="header-containner">
+        <div  className="header-contents">
+            <div  className="logo">
+
+            </div>
+            <div className="menu">
+            <Menu>       
+        
+            <SubMenu key="SubMenu" icon={<SettingOutlined />} title="Navigation Three - Submenu">
+              <Menu.ItemGroup title="Item 1">
+                <Menu.Item key="setting:1">Option 1</Menu.Item>
+                <Menu.Item key="setting:2">Option 2</Menu.Item>
+              </Menu.ItemGroup>
+              <Menu.ItemGroup title="Item 2">
+                <Menu.Item key="setting:3">Option 3</Menu.Item>
+                <Menu.Item key="setting:4">Option 4</Menu.Item>
+              </Menu.ItemGroup>
+            </SubMenu>        
+          </Menu>
+            </div>
+        </div>
+     </div>
     );
   }
 }
