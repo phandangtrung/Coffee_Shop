a4f2e4bc670ae5847ddf3281d5263752f75037b6
Trung Phan <kaitrung99@gmail.com>
2020-10-14 13:19:59 +0700

    Menu navigation

--- frontend_website/src/features/header/view/index.js
+++ frontend_website/src/features/header/view/index.js
@@ -1,37 +1,56 @@
 import React, { Component } from 'react';
-import { Menu } from 'antd';
 
-
-import {SettingOutlined } from '@ant-design/icons';
-import 'antd/dist/antd.css';
 import "./style.css";
-const { SubMenu } = Menu;
+
+import LeftMenu from './leftmenu'
+import RightMenu from './rightmenu'
+import { Drawer, Button } from 'antd';
 class Header extends Component {
  
+  state = {
+		current: 'mail',
+		visible: false
+	}
+	showDrawer = () => {
+		this.setState({
+			visible: true,
+		});
+	};
+
+	onClose = () => {
+		this.setState({
+			visible: false,
+		});
+	};
   render() {
     return (
-      <div  className="header-containner">
-        <div  className="header-contents">
-            <div  className="logo">
-
-            </div>
-            <div className="menu">
-            <Menu>       
-        
-            <SubMenu key="SubMenu" icon={<SettingOutlined />} title="Navigation Three - Submenu">
-              <Menu.ItemGroup title="Item 1">
-                <Menu.Item key="setting:1">Option 1</Menu.Item>
-                <Menu.Item key="setting:2">Option 2</Menu.Item>
-              </Menu.ItemGroup>
-              <Menu.ItemGroup title="Item 2">
-                <Menu.Item key="setting:3">Option 3</Menu.Item>
-                <Menu.Item key="setting:4">Option 4</Menu.Item>
-              </Menu.ItemGroup>
-            </SubMenu>        
-          </Menu>
-            </div>
+      <nav className="menuBar">
+      <div className="logo">
+        <a href="">COFFEE SHOP</a>
+      </div>
+      <div className="menuCon" >
+        <div className="leftMenu">
+          <LeftMenu />
         </div>
-     </div>
+        <div className="rightMenu">
+          <RightMenu />
+        </div>
+        <Button className="barsMenu" type="primary" onClick={this.showDrawer}>
+          <span className="barsBtn"></span>
+        </Button>
+        <Drawer
+          title="Basic Drawer"
+          placement="right"
+          closable={false}
+          onClose={this.onClose}
+          visible={this.state.visible}
+        >
+          <LeftMenu />
+          <RightMenu />
+        </Drawer>
+
+      </div>
+    </nav>
     );
   }
 }
