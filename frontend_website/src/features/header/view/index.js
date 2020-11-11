7c9da2c73207f18bed4e4fe4ca93c10f83bd36ba
Trung Phan <kaitrung99@gmail.com>
2020-11-11 17:19:37 +0700

    Sign up Modal

--- frontend_website/src/features/header/view/index.js
+++ frontend_website/src/features/header/view/index.js
@@ -18,8 +18,13 @@ import {
   TreeSelect,
   Cascader,
   Switch,
+  Modal,
 } from "antd";
 const { Option } = Select;
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 16 },
+};
 const Header = () => {
   const [state, setState] = useState({
     current: "mail",
@@ -88,102 +93,29 @@ const Header = () => {
           </Drawer>
         </div>
       </nav>
-      {/* Sign Up Drawer Form */}
-      <Drawer
-        title="Create a new account"
-        width={720}
-        onClose={onCloseSignup}
+      <Modal
+        title="ĐĂNG KÍ"
         visible={state.Signupvisible}
-        bodyStyle={{ paddingBottom: 80 }}
-        footer={
-          <div
-            style={{
-              textAlign: "right",
-            }}
-          >
-            <Button
-              onClick={onCloseSignup}
-              style={{ marginRight: 8, zoom: "1.5" }}
-            >
-              Cancel
-            </Button>
-            <Button
-              style={{
-                border: "0px",
-                zoom: "1.5",
-              }}
-              onClick={onCloseSignup}
-              type="primary"
-            >
-              Submit
-            </Button>
-          </div>
-        }
+        onOk={showSignup}
+        onCancel={onCloseSignup}
       >
-        <Form
-          labelCol={{ span: 4 }}
-          wrapperCol={{ span: 14 }}
-          layout="horizontal"
-          initialValues={{ size: componentSize }}
-          onValuesChange={onFormLayoutChange}
-          size={componentSize}
-        >
-          <Form.Item label="Form Size" name="size">
-            <Radio.Group>
-              <Radio.Button value="small">Small</Radio.Button>
-              <Radio.Button value="default">Default</Radio.Button>
-              <Radio.Button value="large">Large</Radio.Button>
-            </Radio.Group>
-          </Form.Item>
-          <Form.Item label="Input">
+        <Form {...layout}>
+          <Form.Item
+            label="User name"
+            name="username"
+            rules={[{ required: true }]}
+          >
             <Input />
           </Form.Item>
-          <Form.Item label="Select">
-            <Select>
-              <Select.Option value="demo">Demo</Select.Option>
-            </Select>
-          </Form.Item>
-          <Form.Item label="TreeSelect">
-            <TreeSelect
-              treeData={[
-                {
-                  title: "Light",
-                  value: "light",
-                  children: [{ title: "Bamboo", value: "bamboo" }],
-                },
-              ]}
-            />
-          </Form.Item>
-          <Form.Item label="Cascader">
-            <Cascader
-              options={[
-                {
-                  value: "zhejiang",
-                  label: "Zhejiang",
-                  children: [
-                    {
-                      value: "hangzhou",
-                      label: "Hangzhou",
-                    },
-                  ],
-                },
-              ]}
-            />
-          </Form.Item>
-          <Form.Item label="DatePicker">
-            <DatePicker />
-          </Form.Item>
-          <Form.Item label="InputNumber">
-            <InputNumber />
-          </Form.Item>
-          <Form.Item label="Switch">
-            <Switch />
-          </Form.Item>
-          <Form.Item label="Button">
-            <Button>Button</Button>
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true }]}
+          >
+            <Input />
           </Form.Item>
         </Form>
-      </Drawer>
+      </Modal>
     </div>
   );
 };
