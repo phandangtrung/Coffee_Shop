57365b26d52f0dc4227fb1fc97fa797dbccdb06b
Trung Phan <kaitrung99@gmail.com>
2020-11-11 09:26:33 +0700

    Short_Banner

--- frontend_website/src/config/image.js
+++ frontend_website/src/config/image.js
@@ -1,6 +1,7 @@
 const IMAGE_URL = process.env.PUBLIC_URL;
 
 export const Images = {
+  SBANNER: IMAGE_URL + "/static/img/short_banner.jpg",
   SLIDE1: IMAGE_URL + "/static/img/slider1.png",
   SLIDE2: IMAGE_URL + "/static/img/slider2.png",
   SLIDE3: IMAGE_URL + "/static/img/slider3.png",
--- frontend_website/src/features/product/view/index.js
+++ frontend_website/src/features/product/view/index.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Menu, Row, Col } from "antd";
 import "./style.css";
 import ProductTag from "../../../components/ProductTag";
+import ShortBanner from "../../../components/ShortBanner";
 
 import data from "../dummy";
 
@@ -11,47 +12,50 @@ function Product() {
     console.log("click ", e);
   };
   return (
-    <div className="container">
-      <div className="menu-container">
-        <Menu
-          onClick={handleClick}
-          style={{ width: 256 }}
-          defaultSelectedKeys={["1"]}
-          defaultOpenKeys={["sub1"]}
-          mode="inline"
-        >
-          <Menu.Item className="category-title">DANH MỤC</Menu.Item>
-          {data.map((category) => {
-            return <Menu.Item key={category.id}>{category.name}</Menu.Item>;
-          })}
-        </Menu>
-      </div>
+    <>
+      <ShortBanner />
+      <div className="container">
+        <div className="menu-container">
+          <Menu
+            onClick={handleClick}
+            style={{ width: 256 }}
+            defaultSelectedKeys={["1"]}
+            defaultOpenKeys={["sub1"]}
+            mode="inline"
+          >
+            <Menu.Item className="category-title">DANH MỤC</Menu.Item>
+            {data.map((category) => {
+              return <Menu.Item key={category.id}>{category.name}</Menu.Item>;
+            })}
+          </Menu>
+        </div>
 
-      <div className="product-container">
-        <Row>
-          <Col span={8}>
-            <ProductTag />
-          </Col>
-          <Col span={8}>
-            <ProductTag />
-          </Col>
-          <Col span={8}>
-            <ProductTag />
-          </Col>
-        </Row>
-        <Row>
-          <Col span={8}>
-            <ProductTag />
-          </Col>
-          <Col span={8}>
-            <ProductTag />
-          </Col>
-          <Col span={8}>
-            <ProductTag />
-          </Col>
-        </Row>
+        <div className="product-container">
+          <Row>
+            <Col span={8}>
+              <ProductTag />
+            </Col>
+            <Col span={8}>
+              <ProductTag />
+            </Col>
+            <Col span={8}>
+              <ProductTag />
+            </Col>
+          </Row>
+          <Row>
+            <Col span={8}>
+              <ProductTag />
+            </Col>
+            <Col span={8}>
+              <ProductTag />
+            </Col>
+            <Col span={8}>
+              <ProductTag />
+            </Col>
+          </Row>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
