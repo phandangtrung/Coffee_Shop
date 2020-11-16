cd6943540120a7c61230aed0de19b1a84fe40adf
Trung Phan <kaitrung99@gmail.com>
2020-11-16 18:57:27 +0700

    Comment form

--- frontend_website/src/features/single_product/view/index.js
+++ frontend_website/src/features/single_product/view/index.js
@@ -1,8 +1,17 @@
 import React from "react";
 import "./style.css";
-import { Row, Col, Button, Form, InputNumber } from "antd";
+import {
+  Row,
+  Col,
+  Button,
+  Form,
+  InputNumber,
+  Tabs,
+  Rate,
+  Pagination,
+} from "antd";
 import { Images } from "../../../config/image";
-
+const { TabPane } = Tabs;
 function SingleProduct() {
   const handleSubmit = (values) => {
     console.log("Value: ", values);
@@ -14,7 +23,7 @@ function SingleProduct() {
     <div className="container">
       <div className="sproduct-form">
         <Row>
-          <Col span={12}>
+          <Col style={{ textAlign: "start" }} span={12}>
             <img alt="single-product" src={Images.COCF} />
           </Col>
           <Col span={12}>
@@ -50,6 +59,53 @@ function SingleProduct() {
               </Form.Item>
             </Form>
           </Col>
+        </Row>
+        <Row>
+          <div className="tabs-rating">
+            <Tabs type="card" size="large">
+              <TabPane tab="Rate and Comment" key="1">
+                <div className="rate-container">
+                  <div className="rate-form">
+                    <div className="rate-username">Nguyễn Hùng Duy</div>
+                    <div className="rate-start">
+                      <Rate disabled defaultValue={5} />
+                    </div>
+                    <div className="rate-comment">
+                      Cafe ngon, đúng điệu cafe, sẽ ủng hộ dài dài.
+                    </div>
+                  </div>
+                  <div className="rate-form">
+                    <div className="rate-username">Phạm Quốc Việt</div>
+                    <div className="rate-start">
+                      <Rate disabled defaultValue={4} />
+                    </div>
+                    <div className="rate-comment">
+                      Cà phê ngon, giao nhanh, nhưng chưa được đậm đà. Mong shop
+                      tiếp tục phát triển chất lượng cà phê của mình.
+                    </div>
+                  </div>
+                  <div className="rate-form">
+                    <div className="rate-username">Đỗ Phi Cường</div>
+                    <div className="rate-start">
+                      <Rate disabled defaultValue={1} />
+                    </div>
+                    <div className="rate-comment">
+                      Quán này của đối thủ mình nên mình đánh giá 1 sao, dù Cà
+                      phê quán rất ngon.
+                    </div>
+                  </div>
+                </div>
+                <div className="pagination-form">
+                  <Pagination defaultCurrent={1} total={50} />
+                </div>
+              </TabPane>
+              <TabPane tab="Your Review" key="3">
+                <p>Content of Tab Pane 3</p>
+                <p>Content of Tab Pane 3</p>
+                <p>Content of Tab Pane 3</p>
+              </TabPane>
+            </Tabs>
+          </div>
         </Row>
       </div>
     </div>
