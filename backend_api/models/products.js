4a1d618493a517257763df9677ca8dd510775c34
PhiCuong1603 <56375980+PhiCuong1603@users.noreply.github.com>
2020-11-21 15:19:25 +0700

    updateModels

--- backend_api/models/products.js
+++ backend_api/models/products.js
@@ -6,7 +6,6 @@ const productsSchema = new mongoose.Schema({
     prices: {type: Number, require: true},
     quantity: {type: Number, require: true},
     description: {type: String, require: false},   
-    //categoryId: {type: String, required:true}
     images: {type: String, require: false}
 });
 
--- backend_api/models/shippers.js
+++ backend_api/models/shippers.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const shippersSchema = new mongoose.Schema({
     name: {type: String, require: true, unique: true},
+    images: {type: String, required: false},
     phone: {type: Number, require: false},
     point: {type: Number, require: false},
     //images: {type: String, require: false}
