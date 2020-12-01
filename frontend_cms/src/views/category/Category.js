1356c03e92e323ec9a02161f387e9c7e0b7f6dd7
Trung Phan <kaitrung99@gmail.com>
2020-12-01 16:39:17 +0700

    category show

--- frontend_cms/src/views/category/Category.js
+++ frontend_cms/src/views/category/Category.js
@@ -1,4 +1,4 @@
-import React, { lazy } from "react";
+import React, { lazy, useEffect, useReducer, useState } from "react";
 import {
   CButton,
   CBadge,
@@ -8,33 +8,85 @@ import {
   CDataTable,
   CLink,
 } from "@coreui/react";
+import {
+  Table,
+  Space,
+  Spin,
+  Modal,
+  Row,
+  Col,
+  Input,
+  Form,
+  Checkbox,
+  Upload,
+  Select,
+  Button,
+} from "antd";
 import "./style.css";
 import CIcon from "@coreui/icons-react";
-
+import { dataFetchReducer } from "./reducer/index";
+import {
+  doGetList,
+  doGetList_error,
+  doGetList_success,
+} from "./action/actionCreater";
+import categoryApi from "../../api/categoryApi";
 import usersData from "../users/UsersData";
-const fields = [
-  { key: "id", label: "INDEX", _style: { width: "40%" } },
-  { key: "name", label: "NAME", _style: { width: "50%" } },
-  { key: "action", label: "ACTION", _style: { width: "10%" } },
-  // { key: "registered", _style: { width: "40%" } },
-  // "role",
-  // "status",
-];
-const getBadge = (status) => {
-  switch (status) {
-    case "Active":
-      return "success";
-    case "Inactive":
-      return "secondary";
-    case "Pending":
-      return "warning";
-    case "Banned":
-      return "danger";
-    default:
-      return "primary";
-  }
-};
+
 function Category() {
+  const [form] = Form.useForm();
+  const [isvisible, SetVisible] = useState(false);
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+      // render: (text) => <a>{text}</a>,
+    },
+
+    {
+      title: "Action",
+      key: "action",
+      render: (text, record) => (
+        <Space size="middle">
+          <a>Edit</a>
+          <a>Delete</a>
+        </Space>
+      ),
+    },
+  ];
+  const toggle = () => {
+    SetVisible(!isvisible);
+  };
+  const [categoryList, dispatchCategory] = useReducer(dataFetchReducer, {
+    isLoading: false,
+    isError: false,
+    data: [],
+  });
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    const fetchCategoryList = async () => {
+      // dispatch({ type: "FETCH_INIT" });
+      dispatchCategory(doGetList);
+      try {
+        setIsLoading(true);
+        // const params = { _page: 1, _limit: 10 };
+
+        const response = await categoryApi.getAll();
+        console.log("Fetch products succesfully: ", response);
+        // console.log(response.products);
+        // setProductList(response.products);
+        // dispatch({ type: "FETCH_SUCCESS", payload: response.products });
+        dispatchCategory(doGetList_success(response.categories));
+        // console.log(">>>> productlist: ", productList);
+        setIsLoading(false);
+      } catch (error) {
+        console.log("failed to fetch product list: ", error);
+        dispatchCategory(doGetList_error);
+      }
+    };
+    fetchCategoryList();
+  }, []);
   return (
     <>
       <CCard>
@@ -48,52 +100,47 @@ function Category() {
           }}
           shape="pill"
           color="info"
+          onClick={toggle}
         >
           {/* <i style={{ fontSize: "20px" }} class="cil-playlist-add"></i>  */}
           Add Category
         </CButton>
         <CCardBody>
-          <CDataTable
-            items={usersData}
-            fields={fields}
-            striped
-            itemsPerPage={8}
-            pagination
-            scopedSlots={{
-              index: (item) => <td>{item.id}</td>,
-              status: (item) => (
-                <td>
-                  <CBadge color={getBadge(item.status)}>{item.status}</CBadge>
-                </td>
-              ),
-              action: () => (
-                <td style={{ display: "flex", justifyContent: "start" }}>
-                  <div
-                    style={{
-                      display: "flex",
-                      width: "80%",
-                      justifyContent: "space-between",
-                    }}
-                  >
-                    <CLink className="c-subheader-nav-link" href="#">
-                      <CIcon name="cil-pencil" alt="Edit" />
-                      {/* &nbsp;Edit */}
-                    </CLink>
-                    <CLink className="c-subheader-nav-link" href="#">
-                      <CIcon
-                        style={{ color: "red" }}
-                        name="cil-trash"
-                        alt="Delete"
-                      />
-                      {/* &nbsp;Edit */}
-                    </CLink>
-                  </div>
-                </td>
-              ),
-            }}
-          />
+          {isLoading ? (
+            <div style={{ textAlign: "center" }}>
+              <Spin size="large" />
+            </div>
+          ) : (
+            <Table
+              columns={columns}
+              dataSource={categoryList.data}
+              rowKey="_id"
+            />
+          )}
         </CCardBody>
       </CCard>
+      <Modal
+        title="Add Category"
+        visible={isvisible}
+        // onOk={handleOk}
+        onCancel={toggle}
+        style={{ marginTop: "5%" }}
+      >
+        <Form
+          // initialValues={{ size: componentSize }}
+          // onValuesChange={onFormLayoutChange}
+          form={form}
+          size={"large"}
+        >
+          <Row style={{ display: "flex", justifyContent: "space-between" }}>
+            <Col span={24}>
+              <Form.Item name="name">
+                <Input placeholder="Category name" />
+              </Form.Item>
+            </Col>
+          </Row>
+        </Form>
+      </Modal>
     </>
   );
 }
