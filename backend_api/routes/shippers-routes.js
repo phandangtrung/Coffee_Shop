85599038502efe7a751d5e034e5d4071fefe35df
PhiCuong1603 <56375980+PhiCuong1603@users.noreply.github.com>
2020-12-10 18:57:38 +0700

    fixUploadShipper

--- backend_api/routes/shippers-routes.js
+++ backend_api/routes/shippers-routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { check } = require('express-validator');
 
+const uploadfile = require('../middleware/upload');
 const shippersController = require('../controller/shippers-controllers');
 
 const router = express.Router();
@@ -9,13 +10,15 @@ router.get('/',shippersController.getAllShipper);
 router.get('/:pid',shippersController.getshipperById);
 
 
-router.post('/', 
+router.post('/',
+uploadfile.single('images'), 
 [
     check('name').not().isEmpty()
 ]
 ,shippersController.createShipper);
 
 router.patch('/:pid',
+uploadfile.single('images'),
 [
     check('name').not().isEmpty()
 ]
