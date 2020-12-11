3137b62ea84d47951d2faebfd62dbecf2b0b9d5b
Trung Phan <kaitrung99@gmail.com>
2020-12-11 15:26:17 +0700

    api get user

--- frontend_cms/src/api/userApi.js
+++ frontend_cms/src/api/userApi.js
@@ -5,6 +5,14 @@ class UserApi {
     const url = "/api/users/login/admin";
     return axiosClient.post(url, params);
   };
+  getallUser = (token) => {
+    const url = "/api/users";
+    return axiosClient.get(url, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  };
 }
 const userApi = new UserApi();
 export default userApi;
