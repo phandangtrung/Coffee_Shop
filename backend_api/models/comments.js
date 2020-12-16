9319287eac2533655caa4e4b589c8c0b65694c01
PhiCuong1603 <56375980+PhiCuong1603@users.noreply.github.com>
2020-12-16 11:02:03 +0700

    comment

--- backend_api/models/comments.js
+++ backend_api/models/comments.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const commentsSchema = new mongoose.Schema({
   email: { type: String, require: false },
   content: { type: String, required: false },
+  rating: {type: Number, required: false},
   productId: { type: String, required: false },
 });
 
