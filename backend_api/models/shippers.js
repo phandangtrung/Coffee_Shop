d26ab4c876ec7e3303a6550edf06baa74989e473
PhiCuong1603 <56375980+PhiCuong1603@users.noreply.github.com>
2020-12-18 17:25:07 +0700

    phone

--- backend_api/models/shippers.js
+++ backend_api/models/shippers.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const shippersSchema = new mongoose.Schema({
   name: { type: String, require: false, unique: false },
   imagesShipper: { type: String, required: false },
-  phone: { type: Number, require: false },
+  phone: { type: String, require: false },
   point: { type: Number, require: false },
   createAt: { type: String, require: false },
 });
