dc99efafa5a403f69692a288417dbd2819dca709
PhiCuong1603 <56375980+PhiCuong1603@users.noreply.github.com>
2020-12-20 19:57:14 +0700

    couponCode

--- backend_api/controller/couponCode-controllers.js
+++ backend_api/controller/couponCode-controllers.js
@@ -1,11 +1,14 @@
 const CouponCode = require("../models/couponcode");
 const cc = require("../coupon-code");
 
+const HttpError = require("../error-handle/http-error");
+
 const createCouponCode = async (req, res, next) => {
-  const code = cc.generate;
+  let code = cc.generate();
   const createCouponCode = {
     discount: req.body.discount,
     couponCode: code,
+    note: req.body.note,
   };
   const newDiscount = new CouponCode(createCouponCode);
   await newDiscount.save();
@@ -15,5 +18,66 @@ const createCouponCode = async (req, res, next) => {
   });
 };
 
+const deleteCouponCode = async (req, res, next) => {
+  const CouponCodeId = req.params.ccid;
+  let couponcode;
+  try {
+    couponcode = await CouponCode.findByIdAndDelete(CouponCodeId);
+  } catch (err) {
+    const error = new HttpError("Something went wrong, can not delete", 500);
+    return next(error);
+  }
+  if (!couponcode) {
+    const error = new HttpError("Could not find any Couponcode", 404);
+    return next(error);
+  }
+  res.status(200).json({ message: "Deleted Couponcode successfull" });
+};
 
-module.exports = { createCouponCode };
+const getAllCouponCode = async (req, res, next) => {
+  let couponcode;
+  try {
+    couponcode = await CouponCode.find();
+    console.log(couponcode);
+  } catch (err) {
+    const error = new HttpError(
+      "Something went wrong, coud not find any CouponCode",
+      500
+    );
+    return next(error);
+  }
+
+  if (!couponcode) {
+    const error = new HttpError("Could not find any CouponCode", 404);
+    return next(error);
+  }
+  res.status(200).json({ couponcode });
+};
+
+const getCouponbyCode = async (req, res, next) => {
+  let couponcode;
+  const code = req.body.couponCode;
+  try {
+    couponcode = await CouponCode.findOne({ couponCode: code });
+    console.log(couponcode);
+  } catch (err) {
+    const error = new HttpError(
+      "Something went wrong, coud not find any CouponCode",
+      500
+    );
+    return next(error);
+  }
+
+  if (!couponcode) {
+    const error = new HttpError("Could not find any CouponCode", 404);
+    return next(error);
+  }
+  res.status(200).json({ couponcode });
+};
+
+module.exports = {
+  createCouponCode,
+  deleteCouponCode,
+  getAllCouponCode,
+  getCouponbyCode,
+};
--- backend_api/models/couponcode.js
+++ backend_api/models/couponcode.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const couponCodeSchema = new mongoose.Schema({
+  note: { type: String, required: false },
   discount: { type: Number, require: false },
   couponCode: { type: String, required: false },
 });
