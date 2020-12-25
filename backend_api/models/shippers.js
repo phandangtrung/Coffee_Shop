1195b9924b8fcf67878376a13a4d6927731fffe8
PhiCuong1603 <56375980+PhiCuong1603@users.noreply.github.com>
2020-12-25 18:46:00 +0700

    updateShipper&Order

--- backend_api/models/shippers.js
+++ backend_api/models/shippers.js
@@ -1,10 +1,11 @@
 const mongoose = require("mongoose");
 
 const shippersSchema = new mongoose.Schema({
-  name: { type: String, require: false, unique: false },
+  name: { type: String, require: false },
   imagesShipper: { type: String, required: false },
   phone: { type: String, require: false },
   point: { type: Number, require: false },
+  status: { type: Boolean, default: false },
   createAt: { type: String, require: false },
 });
 
