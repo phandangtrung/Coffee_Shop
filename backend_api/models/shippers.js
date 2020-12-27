c331157e714454534671cac01535b0ecc653737b
PhiCuong1603 <56375980+PhiCuong1603@users.noreply.github.com>
2020-12-27 09:57:49 +0700

    modelShipper

--- backend_api/models/shippers.js
+++ backend_api/models/shippers.js
@@ -1,12 +1,8 @@
 const mongoose = require("mongoose");
 
 const shippersSchema = new mongoose.Schema({
-<<<<<<< HEAD
-  name: { type: String, require: false },
-=======
   name: { type: String, required: false, index: { unique: false } },
   identityCard: { type: String, required: false, index: { unique: true } },
->>>>>>> feature/Cuong
   imagesShipper: { type: String, required: false },
   phone: { type: String, require: false },
   point: { type: Number, require: false },
