5cd4cfa2049cebeecb992f35195dbfc00a9871a6
Trung Phan <kaitrung99@gmail.com>
2021-05-19 11:05:54 +0700

    no message

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,22 @@
 import logo from "./logo.svg";
-import React from "react";
+import React, { useState } from "react";
 import GoogleLogin from "react-google-login";
 import FacebookLogin from "react-facebook-login";
 import axios from "axios";
 
 function App() {
+  const [isloading, setIsloading] = useState(false);
+  const [userTk, setuserTk] = useState("");
   const responseSuccessGoogle = (response) => {
-    console.log(response);
+    setIsloading(true);
+    console.log(">> này nè", response);
     axios({
       method: "POST",
       url: "http://localhost:5000/api/users/googlelogin",
       data: { tokenId: response.tokenId },
     }).then((response) => {
       console.log("Google Login Success", response);
+      setIsloading(false);
     });
   };
 
@@ -35,13 +39,17 @@ function App() {
     <div className="App">
       <div>
         <h1> Login with GOOGLE</h1>
-        <GoogleLogin
-          clientId="161356782679-supo9tgvceuf5u8ts0d0su6d3eg4sckf.apps.googleusercontent.com"
-          buttonText="Login with google"
-          onSuccess={responseSuccessGoogle}
-          onFailure={responseErrorGoogle}
-          cookiePolicy={"single_host_origin"}
-        />
+        {isloading ? (
+          <div>Đang đăng nhập...</div>
+        ) : (
+          <GoogleLogin
+            clientId="161356782679-supo9tgvceuf5u8ts0d0su6d3eg4sckf.apps.googleusercontent.com"
+            buttonText="Login with google"
+            onSuccess={responseSuccessGoogle}
+            onFailure={responseErrorGoogle}
+            cookiePolicy={"single_host_origin"}
+          />
+        )}
       </div>
 
       <div>
