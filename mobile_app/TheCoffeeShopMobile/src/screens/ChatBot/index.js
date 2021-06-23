1b9c934dab0e609173c074127479447ff6003080
phandangtrung <kaitrung99@gmail.com>
2021-06-23 20:47:20 +0700

    chatbot mobile

--- mobile_app/TheCoffeeShopMobile/src/screens/ChatBot/index.js
+++ mobile_app/TheCoffeeShopMobile/src/screens/ChatBot/index.js
@@ -2,19 +2,41 @@ import React, {useState, useEffect, useCallback} from 'react';
 import {SafeAreaView, View, Text, Image} from 'react-native';
 import {GiftedChat} from 'react-native-gifted-chat';
 import {Dialogflow_V2} from 'react-native-dialogflow';
-
+import {Bubble} from 'react-native-gifted-chat';
+import uuid from 'react-native-uuid';
 import {dialogflowConfig} from '../../config/env';
 function ChatBot() {
   const [messages, setMessages] = useState([]);
   const BOT_USER = {
     _id: 2,
     name: 'FAQ Bot',
-    avatar: 'https://i.imgur.com/7k12EPD.png',
+    avatar:
+      'https://st3.depositphotos.com/8950810/17657/v/600/depositphotos_176577870-stock-illustration-cute-smiling-funny-robot-chat.jpg',
+  };
+  const renderBubble = (props) => {
+    return (
+      <Bubble
+        {...props}
+        textStyle={{
+          right: {
+            color: 'white',
+          },
+        }}
+        wrapperStyle={{
+          // left: {
+          //   backgroundColor: 'white',
+          // },
+          right: {
+            backgroundColor: 'orange',
+          },
+        }}
+      />
+    );
   };
   useEffect(() => {
     setMessages([
       {
-        _id: 20,
+        _id: 1,
         text: `The Coffee Shop xin chào quý khách ạ.`,
         createdAt: new Date(),
         user: BOT_USER, // <= note this
@@ -29,7 +51,7 @@ function ChatBot() {
   }, []);
   const sendBotResponse = (text) => {
     let msg = {
-      _id: messages.length + 1,
+      _id: uuid.v4(),
       text,
       createdAt: new Date(),
       user: BOT_USER,
@@ -64,6 +86,7 @@ function ChatBot() {
       <GiftedChat
         messages={messages}
         onSend={(messages) => onSend(messages)}
+        renderBubble={renderBubble}
         user={{
           _id: 1,
         }}
